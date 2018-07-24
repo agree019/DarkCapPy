3a67a8c5dc15d9820c1218518a2ead91c0971bba
Adam Green <agree019@ucr.edu>
2018-07-24 08:50:39 -0700

    Delete ModelData.py

--- DarkCapPy/Configure/ModelData.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import scipy as sp
-import pandas as pd
-from scipy import interpolate
-import scipy.integrate as integrate
-import time
-
-import matplotlib as mpl
-import matplotlib.pyplot as plt
